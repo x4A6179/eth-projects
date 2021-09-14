72e01f6700815e7513d58c5f77e415709e908e41
x4A6179 <nice.s0ck5@protonmail.com>
2021-09-13 23:44:01 -0400

    added dependencies; started skeletion on building lease agreement smart contract

---
+++ nft/REtoken/contracts/lease.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.7.0;
+
+import '@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol'
+import '@openzeppelin/contracts/access/Ownable.sol'
+
+contract LeaseAgreement is Ownable, ERC721PresetMinterPauserAutoId {
+  // need to include something to hold who (addresses) involved with contract
+  mapping(address => bool) private partiesInvolved;
+  // constructor to create LeaseAgreement (possible called by manager)
+    // possible
+  constructor (string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
+  // function to accept LeaseAgreement (called by manager)
+  // function to sign LeaseAgreement (would be called by the signee)
+  // revert LeaseAgreement on where conditions arent met (possible test)
+}
