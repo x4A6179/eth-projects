a434e1b3f46f0df6b5a1bdd83a67979191eca309
x4A6179 <afraites@vt.edu>
2021-09-14 23:45:03 -0400

    added storage variables; started building out functions for landlord operations

--- nft/REtoken/contracts/lease.sol
+++ nft/REtoken/contracts/lease.sol
@@ -1,15 +1,34 @@
-pragma solidity ^0.7.0;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
 
-import '@openzeppelin/contracts/token/ERC721/presets/ERC721Enumerable.sol'
-import '@openzeppelin/contracts/token/ERC721/presets/ERC721Burnable.sol'
-import '@openzeppelin/contracts/access/Ownable.sol'
+import '@openzeppelin/contracts/token/ERC721/presets/ERC721Enumerable.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+import 'openzeppelin/contracts/utils/math/SafeMath.sol';
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract LeaseAgreement is Ownable, ERC721Burnable, ERC721Enumerable {
   // need to include something to hold who (addresses) involved with contract
-  mapping(address => bool) private partiesInvolved;
+  using SafeMath for uint256;
+  bytes32 public constant LANDLORD_ROLE = keccak256("LANDLORD_ROLE");
+  bytes32 public constant LEASEE_ROLE = keccak256("LEASEE_ROLE");
+  mapping(address[] => uint256) private partiesInvolved;
+  uint256 public maxPartiesInvolved = 4;
+  address[] private totalInvolved;
+  uint256 private leasePrice;
+
   // constructor to create LeaseAgreement (possible called by manager)
     // possible
   constructor (string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
+  // function setLeasePrice (called by manager role)
+  function setLeasePrice (uint256 _price) external onlyRole(LANDLORD_ROLE) {
+    leasePrice = _price;
+  }
+  function addToLease (address[] leasees) external onlyRole(LANDLORD_ROLE) {
+    for (uint256 i = 0; i < leasees.length; i++){
+      require(leasees[i] != address(0), "Not able to add null address to lease agreement")
+      totalInvolved.push(leasees[i]);
+    }
+  }
   // function to accept LeaseAgreement (called by manager)
   // function to sign LeaseAgreement (would be called by the signee)
   // revert LeaseAgreement on where conditions arent met (possible test)
