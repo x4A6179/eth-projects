659112fce2c50eced983a6ecf6e51c500b129052
x4A6179 <afraites@vt.edu>
2021-09-16 00:08:27 -0400

    need to complete removeFromLease

--- nft/REtoken/contracts/lease.sol
+++ nft/REtoken/contracts/lease.sol
@@ -3,16 +3,21 @@ pragma solidity ^0.8.0;
 
 import '@openzeppelin/contracts/token/ERC721/presets/ERC721Enumerable.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
-import 'openzeppelin/contracts/utils/math/SafeMath.sol';
+import '@openzeppelin/contracts/utils/math/SafeMath.sol';
 import '@openzeppelin/contracts/access/AccessControl.sol';
 
-contract LeaseAgreement is Ownable, ERC721Burnable, ERC721Enumerable {
+contract LeaseAgreement is Ownable, ERC721Enumerable {
   // need to include something to hold who (addresses) involved with contract
   using SafeMath for uint256;
+
   bytes32 public constant LANDLORD_ROLE = keccak256("LANDLORD_ROLE");
   bytes32 public constant LEASEE_ROLE = keccak256("LEASEE_ROLE");
-  mapping(address[] => uint256) private partiesInvolved;
+
+  mapping(address => bool) private isPartyInvolved;
+
   uint256 public maxPartiesInvolved = 4;
+  uint256 public totalSupply = 1;
+
   address[] private totalInvolved;
   uint256 private leasePrice;
 
@@ -25,11 +30,21 @@ contract LeaseAgreement is Ownable, ERC721Burnable, ERC721Enumerable {
   function setLeasePrice (uint256 _price) external onlyRole(LANDLORD_ROLE) {
     leasePrice = _price;
   }
-  function addToLease (address[] leasees) external onlyRole(LANDLORD_ROLE) {
-    for (uint256 i = 0; i < leasees.length; i++){
-      require(leasees[i] != address(0), "Not able to add null address to lease agreement")
+
+  function addToLease (address[] leasees, uint256 numberOfDays) external onlyRole("LANDLORD_ROLE") {
+    for (uint256 i = 0; i < leasees.length; i++) {
+      require(leasees[i] != address(0), "Not able to add null address to lease agreement");
+      require(totalInvolved.length < maxPartiesInvolved, "Unable to add another address");
+      isPartyInvolved[leasees[i]] = true;
       totalInvolved.push(leasees[i]);
     }
+  }
+
+  function removeFromLease (address[] leasees) external onlyRole("LANDLORD_ROLE") {
+    for (uint256 i = 0; i < leasees.length; i++) {
+      require(leasees[i] != address(0), "Can not interact with null address");
+      require(totalInvolved.length > 0, "No others involved in contract");
+      // need to finish function; should remove the enter addresses from contract list
   }
   // function to accept LeaseAgreement (called by manager)
   // function to sign LeaseAgreement (would be called by the signee)
