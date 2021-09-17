d4decd9791a372c218c1f12fa12e8b1bffce6f99
x4A6179 <afraites@vt.edu>
2021-09-16 23:38:34 -0400

    restructed storage variables; few core functions finished (add/remove/lookup)

--- nft/REtoken/contracts/lease.sol
+++ nft/REtoken/contracts/lease.sol
@@ -11,41 +11,57 @@ contract LeaseAgreement is Ownable, ERC721Enumerable {
   using SafeMath for uint256;
 
   bytes32 public constant LANDLORD_ROLE = keccak256("LANDLORD_ROLE");
-  bytes32 public constant LEASEE_ROLE = keccak256("LEASEE_ROLE");
+  bytes32 public constant LEASEE_ROLE = keccak256("INVESTOR_ROLE");
 
-  mapping(address => bool) private isPartyInvolved;
+  struct Investor {
+    uint256 daysInvested;
+    uint256 index;
+  }
 
-  uint256 public maxPartiesInvolved = 4;
+  mapping(address => Investor) private investors;
+  address[] private invested;
+
+  uint256 public maxInvolved = 4;
   uint256 public totalSupply = 1;
+  uint256 public sharePrice;
 
-  address[] private totalInvolved;
-  uint256 private leasePrice;
+  //Events to emit to the blockchain whenever there is a change of contract state
+  event LogNewInvestor(address indexed _address, block.timestamp)
+  event RemovedInvestor (address indexed _address, block.timestamp)
 
   // constructor to create LeaseAgreement (possible called by manager)
     // possible
-  constructor (string memory _name, string memory _symbol) ERC721(_name, _symbol) {
-    _setupRole(DEFAULT_ADMIN_ROLE, msg.sender)
-  }
-  // function setLeasePrice (called by manager role)
-  function setLeasePrice (uint256 _price) external onlyRole(LANDLORD_ROLE) {
-    leasePrice = _price;
+  constructor (string memory _name, string memory _symbol) ERC721(_name, _symbol) { }
+  // function setPrice (called by manager role)
+  function setPrice (uint256 _price) external onlyOwner {
+    sharePrice = _price;
   }
 
-  function addToLease (address[] leasees, uint256 numberOfDays) external onlyRole("LANDLORD_ROLE") {
-    for (uint256 i = 0; i < leasees.length; i++) {
-      require(leasees[i] != address(0), "Not able to add null address to lease agreement");
-      require(totalInvolved.length < maxPartiesInvolved, "Unable to add another address");
-      isPartyInvolved[leasees[i]] = true;
-      totalInvolved.push(leasees[i]);
+  function isInvested (address _address) internal returns (bool status) {
+    require(_address != address(0), "Null Address not allowed in contract"
+    if (invested.length == 0) return false;
+    return (invested[investors[_address].index] == _address);
+  }
+
+  function addInvestor (address _address) external onlyOwner {
+    for (uint256 i = 0; i < investors.length; i++) {
+      require(_address != address(0), "Not able to add null address to agreement");
+      require(invested.length < maxInvolved, "Unable to add another address");
+      Investor[_address].index = invested.push(_address) - 1;
     }
   }
 
-  function removeFromLease (address[] leasees) external onlyRole("LANDLORD_ROLE") {
-    for (uint256 i = 0; i < leasees.length; i++) {
-      require(leasees[i] != address(0), "Can not interact with null address");
-      require(totalInvolved.length > 0, "No others involved in contract");
-      // need to finish function; should remove the enter addresses from contract list
-  }
+  function removeInvestor (address _address) external onlyOwner {
+    require(_address != address(0), "Unable to interact with null address");
+    require(totalInvolved.length > 0, "Unable to remove. Contract is empty");
+    if (!isInvested(_address)) throw;
+    uint256 removedIndex = investors[_address].index;
+    address swappedAddr = invested[invested.length - 1];
+    invested[removedIndex] = swappedAddr;
+    invested[swappedAddr] = removedIndex
+    invested.length--;
+
+
   // function to accept LeaseAgreement (called by manager)
   // function to sign LeaseAgreement (would be called by the signee)
   // revert LeaseAgreement on where conditions arent met (possible test)
