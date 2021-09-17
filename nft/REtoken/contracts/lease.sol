a7b1af3a21ce07a559480850bcccb38268ee626b
x4A6179 <afraites@vt.edu>
2021-09-17 00:27:39 -0400

    cleaned up code, refactored events

--- nft/REtoken/contracts/lease.sol
+++ nft/REtoken/contracts/lease.sol
@@ -26,8 +26,8 @@ contract LeaseAgreement is Ownable, ERC721Enumerable {
   uint256 public sharePrice;
 
   //Events to emit to the blockchain whenever there is a change of contract state
-  event LogNewInvestor(address indexed _address, block.timestamp)
-  event RemovedInvestor (address indexed _address, block.timestamp)
+  event LogNewInvestor(address indexed _address)
+  event RemovedInvestor (address indexed _address)
 
   // constructor to create LeaseAgreement (possible called by manager)
     // possible
@@ -45,15 +45,18 @@ contract LeaseAgreement is Ownable, ERC721Enumerable {
 
   function addInvestor (address _address) external onlyOwner {
     for (uint256 i = 0; i < investors.length; i++) {
-      require(_address != address(0), "Not able to add null address to agreement");
-      require(invested.length < maxInvolved, "Unable to add another address");
-      Investor[_address].index = invested.push(_address) - 1;
+      require(_address != address(0), "Not able to add null address to agreement.");
+      require(invested.length < maxInvolved, "Unable to add another address.");
+      require(!isInvested, "Address already invested");
+      investors[_address].index = invested.push(_address) - 1;
+      investors[_address].daysInvested = investors[_address].daysInvested > 0 ? investors[_address].daysInvested : 0;
+      emit LogNewInvestor(_address,);
     }
   }
 
   function removeInvestor (address _address) external onlyOwner {
-    require(_address != address(0), "Unable to interact with null address");
-    require(totalInvolved.length > 0, "Unable to remove. Contract is empty");
+    require(_address != address(0), "Unable to interact with null address.");
+    require(totalInvolved.length > 0, "Unable to remove. Contract is empty.");
     if (!isInvested(_address)) throw;
     uint256 removedIndex = investors[_address].index;
     address swappedAddr = invested[invested.length - 1];
@@ -65,4 +68,5 @@ contract LeaseAgreement is Ownable, ERC721Enumerable {
   // function to accept LeaseAgreement (called by manager)
   // function to sign LeaseAgreement (would be called by the signee)
   // revert LeaseAgreement on where conditions arent met (possible test)
+  // need off chain calculation to determine days invested
 }
