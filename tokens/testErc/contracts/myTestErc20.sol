58fdcd4b76c932317f720e6c15fd0a4abc2b94a3
x4A6179 <afraites@vt.edu>
2021-09-19 23:29:05 -0400

    started own implementation of ERC20 token using IERC20 contract

--- tokens/testErc/contracts/myTestErc20.sol
+++ tokens/testErc/contracts/myTestErc20.sol
@@ -1,12 +1,40 @@
 pragma solidity ^0.8.0;
 
-import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 
 // Using this template calls directory from openzeppelin implementation
-contract testErc20 is ERC20, Ownable {
-  constructor(string memory name, string memory symbol) ERC20(name, symbol) {
-    // only function needed to create the token
-    _mint(msg.sender, 100 * 10**uint(decimals()));
+contract testErc20 is IERC20, Ownable {
+  mapping(address => mapping(address => uint256)) private _allowances;
+  mapping(address => uint256) private _balances
+
+  string private _name;
+  string private _symbol;
+
+  constructor(string memory name, string memory symbol) {
+    _name = name;
+    _symbol = symbol;
   }
+
+  function name() public view override return (string memory) {
+    return _name;
+  }
+
+  function symbol() public view override return (string memory) {
+    return _symbol;
+  }
+  
+  function getBalance(address _address) public view return (uint256) {
+    return _balances[_address];
+  }
+
+  function _transfer(address _from, address _to, uint256 amount) internal {
+    require(_from =! address(0), 'Unable to tranfer from zero address.');
+    require(_to =! address(0), 'Unable to send zero address. Call burn function instead.');
+    require(_balances[_from] >= amount, 'Insufficient funds.');
+    _balances[_from] = _balances[_from] - amount;
+    _balances[_to] = _balances[_to] + amount;
+    emit Tranfer(_from, _to, amount);
+  }
+
 }
