a870884e9526254887bb1b1904df6985d0c2d4aa
x4A6179 <afraites@vt.edu>
2021-09-21 23:34:59 -0400

    added safe transfer function and updated existing tranfer function return value

--- tokens/testErc/contracts/myTestErc20.sol
+++ tokens/testErc/contracts/myTestErc20.sol
@@ -10,6 +10,7 @@ contract testErc20 is IERC20, Ownable {
 
   string private _name;
   string private _symbol;
+  uint256 private _totalSupply = 3000; // creating a token with 3k max supply (no burns)
 
   constructor(string memory name, string memory symbol) {
     _name = name;
@@ -23,18 +24,26 @@ contract testErc20 is IERC20, Ownable {
   function symbol() public view override return (string memory) {
     return _symbol;
   }
-  
+
+  function totalSupply() public view override return (uint256) {
+    return _totalSupply;
+  }
+
   function getBalance(address _address) public view return (uint256) {
     return _balances[_address];
   }
 
-  function _transfer(address _from, address _to, uint256 amount) internal {
-    require(_from =! address(0), 'Unable to tranfer from zero address.');
-    require(_to =! address(0), 'Unable to send zero address. Call burn function instead.');
+  function _transfer(address _from, address _to, uint256 amount) internal return (bool) {
+    require(_from == msg.sender);
+    require(_from != address(0), 'Unable to tranfer from zero address.');
+    require(_to != address(0), 'Unable to send zero address. Call burn function instead.');
     require(_balances[_from] >= amount, 'Insufficient funds.');
     _balances[_from] = _balances[_from] - amount;
     _balances[_to] = _balances[_to] + amount;
     emit Tranfer(_from, _to, amount);
+    return true;
   }
 
+  function safeTransferFrom(address _to, uint256 amount) external return ()
+  return _transfer(msg.sender(), _to, amount);
 }
