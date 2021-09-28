7b8837ee97b58d793fa4f7dc7dea1efdec9a013b
x4A6179 <afraites@vt.edu>
2021-09-27 22:10:31 -0400

    refactored deployment test case, finished transfer test case

--- tokens/testErc/test/sampleERC20.js
+++ tokens/testErc/test/sampleERC20.js
@@ -6,9 +6,30 @@ describe("myTestErc20", function() {
     const myTestErc20 = await ethers.getContractFactory("testErc20");
     const testerc20 = await myTestErc20.deploy("DopeMoonToken", "DMT");
     await testerc20.deployed();
+
+    const [owner] = await ethers.getSigners();
     name = await testerc20.name();
     symbol = await testerc20.symbol();
+    //owner = await testerc20.owner();
+    totalSupply = await testerc20.totalSupply();
     expect(name).to.equal("DopeMoonToken");
     expect(symbol).to.equal("DMT");
+    expect(await testerc20.connect(owner).balanceOf(owner.address)).to.equal(totalSupply);
+  });
+  it("Should be able to transfer tokens from owner to another address", async function () {
+    // deploying the contract
+    const myTestErc20 = await ethers.getContractFactory("testErc20");
+    const testErc20 = await myTestErc20.deploy("DopeMoonToken", "DMT");
+    await testErc20.deployed();
+
+    // gets a list of other accounts to which you can test from
+    const [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+
+    // transfering tokens from the owner to another address.
+    initOwnerTokens = await testErc20.balanceOf(owner.address);
+    amount = 5;
+    await testErc20.transfer(addr1.address, amount);
+    expect(await testErc20.balanceOf(addr1.address)).to.equal(amount);
+    expect(await testErc20.balanceOf(owner.address)).to.equal(initOwnerTokens - amount);
   });
 });
