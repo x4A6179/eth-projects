95bda5546d75b48556a963e5c6ff11cb415ade68
x4A6179 <59290923+x4A6179@users.noreply.github.com>
2021-11-04 23:53:24 -0400

    created inital setup/skeleton; configured chains

---
+++ nft-marketplace/hardhat.config.js
@@ -0,0 +1,25 @@
+require("@nomiclabs/hardhat-waffle")
+const fs = require('fs')
+const pk = fs.readFileSync('.env').toString().trim()
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: "https://rpc-mumbai.matic.today",
+      accounts: [pk]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
