973483c6f666bc346783f9823e25860910ce3568
x4A6179 <59290923+x4A6179@users.noreply.github.com>
2021-11-11 23:31:18 -0500

    created ability to list item; made function to make sale; need to add function to view owned tokens

---
+++ nft-marketplace/contracts/Marketplace.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.3;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NFTMarketplace is ReentrancyGuard {
+    using Counters for Counters.Counter;
+    Counters.Counter private _itemIds;
+    Counters.Counter private _itemsSold;
+
+    address payable owner;
+    mapping(uint256 => Item) private idToItem;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    struct Item {
+        uint256 itemId;
+        uint256 tokenId;
+        uint256 price;
+        address tokenContract;
+        address payable seller;
+        address payable owner;
+        bool sold;
+    }
+
+    function getListingPrice(uint256 _itemId) public view returns (uint256 price) {
+        return idToItem[_itemId].price;
+    }
+
+    // process to list item for sale
+    function postForSale(
+        address tokenContract, 
+        uint256 tokenId, 
+        uint256 price) public payable nonReentrant {
+            require(price > 0, "Price must at least be 1 wei");
+            
+            _itemIds.increment();
+            uint256 itemId = _itemIds.current();
+            idToItem[itemId] = Item(
+                itemId,
+                tokenId,
+                price,
+                tokenContract,
+                payable(msg.sender),
+                payable(address(0)),
+                false
+            );
+            IERC721(tokenContract).transferFrom(msg.sender, address(this), tokenId);
+    }
+
+    // process for creating/completing the sale (i.e. transferring)
+    function createSale(
+        address tokenContract,
+        uint256 itemId
+    ) public payable nonReentrant {
+        uint256 listPrice = idToItem[itemId].price;
+        uint256 tokenId = idToItem[itemId].tokenId;
+        require(msg.value == listPrice, "Must send enough funds to match list price");
+
+        idToItem[itemId].seller.transfer(msg.value);
+        IERC721(tokenContract).transferFrom(address(this), msg.sender, tokenId);
+        idToItem[itemId].owner = payable(msg.sender);
+        idToItem[itemId].sold = true;
+        _itemsSold.increment();
+        payable(owner).transfer(listPrice);
+    }
+
+    // process for showing owned tokens
+}
